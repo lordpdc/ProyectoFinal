e588b488513ae6b544eb6a7d0bd37c22255a8c40
Gabriel Puc <gabo.user01@gmail.com>
2016-12-04 18:59:13 -0600

    Splash
    desarrollo del splash en un AsyncTask, en el backgroud se checara su el
    usuario esta logueado o no (falta terminar el login), preparacion para
    el Api de Maps

--- app/src/main/java/computomovil/proyectofinal/LaunchActivity.java
+++ app/src/main/java/computomovil/proyectofinal/LaunchActivity.java
@@ -4,30 +4,64 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class LaunchActivity extends AppCompatActivity {
+public class LaunchActivity extends Activity {
     Intent nexActivity;
     private static final String SESSION="login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SessionManager manager=new SessionManager(this);
-        String user=manager.getPreference(SESSION);
-        if(user.isEmpty()){
-            nexActivity=new Intent(this,Login.class);
-        }
-        else{
-            nexActivity=new Intent(this,Initial_Screen.class);
-
-        }
+        setContentView(R.layout.splash);
+        new Splash().execute();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        startActivity(nexActivity);
+        //startActivity(nexActivity);
     }
+
+    private class Splash extends AsyncTask{
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected Void doInBackground(Object[] params) {
+
+            SessionManager manager=new SessionManager(getApplicationContext());
+            String user=manager.getPreference(SESSION);
+            if(user.isEmpty()){
+                nexActivity=new Intent(getApplicationContext(),Login.class);
+            }
+            else{
+                nexActivity=new Intent(getApplicationContext(),Initial_Screen.class);
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            startActivity(nexActivity);
+            finish();
+        }
+    }
+
 }
+
+
