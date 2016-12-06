fd6fcd48e78517f513ca43d02c635e08a830ca82
Gabriel Puc <gabo.user01@gmail.com>
2016-12-06 12:22:14 -0600

    Inicio de la creacion de circulos
    se puso la creacion de una forma mas decente, ya se puede poner la
    creacion en un ciclo for, ya esta el codigo para probar el giroscopio,
    pero ninguno de mis dispositivos tiene giroscopio

--- app/src/main/java/computomovil/proyectofinal/GameActivity.java
+++ app/src/main/java/computomovil/proyectofinal/GameActivity.java
@@ -1,8 +1,13 @@
 package computomovil.proyectofinal;
 
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,9 +19,11 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 
 
-public class GameActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class GameActivity extends AppCompatActivity implements OnMapReadyCallback,SensorEventListener{
 
     private GoogleMap map;
+    private SensorManager sensorManager;
+    private Sensor gyroscope;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,19 @@ public class GameActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_game);
         MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        gyroscope = sensorManager .getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,gyroscope,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super .onPause();
+        sensorManager .unregisterListener(this);
     }
 
     @Override
@@ -34,17 +54,42 @@ public class GameActivity extends AppCompatActivity implements OnMapReadyCallbac
         CameraUpdate camUpd = CameraUpdateFactory.newLatLngZoom(new LatLng(21.048192, -89.644379), 20);
         map.moveCamera(camUpd);
 
-        CircleOptions circleOptions = new CircleOptions()
+        CircleOptions[] circles = new CircleOptions[4];
+
+        circles[0]= new CircleOptions()
                 .center(new LatLng(21.047866, -89.644431))
                 .radius(4)
                 .strokeColor(Color.CYAN)
                 .fillColor(Color.CYAN);
-        Circle circle = map.addCircle(circleOptions);
+        circles[1] = new CircleOptions()
+                .center(new LatLng(21.047892, -89.644479))
+                .radius(4)
+                .strokeColor(Color.CYAN)
+                .fillColor(Color.CYAN);
+
+        //Circle circle = map.addCircle(circles[0]);
+        //Circle circle2 = map.addCircle(circles[1]);
+        map.addCircle(circles[0]);
+        map.addCircle(circles[1]);
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float []values = event.values ;
+        float x = values[ 0 ];
+        float y = values[ 1 ];
+        float z = values[ 2 ];
+        Toast.makeText(this,""+x+""+y+""+z,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
