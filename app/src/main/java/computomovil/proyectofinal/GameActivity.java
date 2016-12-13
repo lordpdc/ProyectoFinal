dccb75033a5642d944991f2aa47a6fb71a6fb490
lordpdc <lordpdc@hotmail.com>
2016-12-13 13:59:07 -0600

    solucionado error de posicion erronea

--- app/src/main/java/computomovil/proyectofinal/GameActivity.java
+++ app/src/main/java/computomovil/proyectofinal/GameActivity.java
@@ -226,7 +226,7 @@ public class GameActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        currentPosition+=value;
+                        currentPosition+=value+1;
                         setNextPosition(currentPosition);
 
                     }
